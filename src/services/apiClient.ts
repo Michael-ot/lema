4676c63557a2b48e3f188697d1fda9a2cc63137a
michael-ot <michaelotusanya@gmail.com>
2025-07-24 11:23:35 +0100

    Updated ApiClient base url to server's url'

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = "http://owiup-lema.owiup.com/api";
+
+
 const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
